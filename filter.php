60cf4d6b64dcf7463d67e44cc6ea832cd60f74a3
ozeflyer <ozeflyer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-19 19:27:06 +0000

    Adding version 0.7.5
    git-svn-id: https://plugins.svn.wordpress.org/watchmyback24/trunk@41604 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ filter.php
@@ -0,0 +1,28 @@
+<?php
+
+# ----------------------------------------------------------------------------
+# COMMENT FILTER AGAINST BBCODE AND HOORAY JOBS
+# ----------------------------------------------------------------------------
+
+# version:0.7.2
+
+define('WMB_FILTER','0.0.4');
+
+$wmb_comment_filter = array(
+   '[url',
+   'array',
+   'great site',
+   'great job',
+   'nice',
+   'good',
+   'good site',
+   'good post',
+   'thank you',
+   'interest site',
+   'cool',
+   'sorry',
+   'wrote an interesting post today',
+   'sex'
+);
+
+?>
